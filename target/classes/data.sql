14cabde18c04479d4081db0e84a0c0f50216fd1f
danillocesar <danillo.mendonca@gmail.com>
2018-05-25 19:13:37 -0300

    init

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,18 +1,18 @@
 insert into Product
-values(1, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 1990.99);
+values(1, 'https://assets.academy.com/mgen/50/10740350.jpg', 'Shirt 1', 1990.99);
 insert into Product
-values(2, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 4870.90);
+values(2, 'https://www.customink.com/mms/images/catalog/styles/286800/catalog_detail_image_large.jpg', 'Shirt 2', 4870.90);
 insert into Product
-values(3, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 1200.00);
+values(3, 'https://www.customink.com/mms/images/catalog/styles/271900/catalog_detail_image_large.jpg', 'Shirt 3', 1200.00);
 insert into Product
-values(4, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 140.49);
+values(4, 'https://img.michaels.com/L6/3/IOGLO/873480049/212543230/10093625_r.jpg?fit=inside|1024:1024', 'Shirt 4', 140.49);
 insert into Product
-values(5, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 122.22);
+values(5, 'http://brandstore.vistaprint.in/render/undecorated/product/PVAG-0Q4K507W3K1Y/RS-K82Q06W655QY/jpeg?compression=95&width=700', 'Shirt 5', 122.22);
 insert into Product
-values(6, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 999.99);
+values(6, 'https://static4.cilory.com/273124-large_default/nologo-navy-casual-shirt.jpg', 'Shirt 6', 999.99);
 insert into Product
-values(7, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 59.00);
+values(7, 'https://static7.cilory.com/257037-thickbox_default/no-logo-sky-blue-casual-shirt.jpg', 'Shirt 7', 59.00);
 insert into Product
-values(8, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 450.00);
+values(8, 'http://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwb2310ab1/images/hi-res/52931_DVNB.jpg?sw=750&sh=750&sm=fit&sfrm=png', 'Shirt 8', 450.00);
 insert into Product
-values(9, 'https://www.w3schools.com/howto/img_avatar.png', 'Shirt', 399.90);
+values(9, 'https://gear.blizzard.com/media/catalog/product/cache//550x550/a4e40ebdc3e371adff845072e1c73f37/o/v/overwatch-long-sleeve-shirt.png', 'Shirt 9', 399.90);
